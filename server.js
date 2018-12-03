9ada412cfa65965657bbcf5168d6f0a1d815b0ba
calm <calmdusk615@gmail.com>
2018-12-04 00:10:40 +0100

    added

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ app.use(function (req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
    });
-app.use(express.static(__dirname + '/dist'));
+app.get('/*all', function(req, res){
+    rew.sendFile(path.join(__dirname + '/dist'));
+})
+app.use(express.static(__dirname + '/dist/calmdusk/index.html'));
 
 app.post("/send-mail", (req, res)=> {
     setTimeout( function(){
