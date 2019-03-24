4322d72f388b0277db51f59611cc8e92e877faba
calm <calmdusk615@gmail.com>
2019-03-24 23:37:44 +0100

    ada

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
 
    app.use(express.static(path.join(__dirname, 'dist')));
    app.get('*', function(req, res) {
-       res.sendfile('./dist/index.html');
+       res.sendfile('./dist/calmdusk/index.html');
    });
 
 
