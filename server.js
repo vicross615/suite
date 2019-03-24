074c6c7cd185901953dd1eade7baad49536bf05b
calm <calmdusk615@gmail.com>
2019-03-24 19:10:35 +0100

    ada

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(function (req, res, next) {
    });
 
 app.use(express.static(__dirname + '/dist/calmdusk/index.html'));
-app.get('/', (req, res) => {
-    res.sendfile('/dist/calmdusk/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/calmdusk/index.html'));
 });
 
 
