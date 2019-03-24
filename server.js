5394d4f1ad5e9630e2203043855c37cfc0d2bc85
calm <calmdusk615@gmail.com>
2019-03-24 17:44:18 +0100

    ada

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(function (req, res, next) {
    });
 
 app.use(express.static(__dirname + '/dist/calmdusk/index.html'));
-app.get('*', (req, res) => {
-    res.sendFile('/dist/calmdusk/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
+// app.get('*', (req, res) => {
+//     res.sendfile('/dist/calmdusk/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+// });
 
 
 
